318750edf65a27453b7e980893301d1cf17a4bb3
martoon01 <martoonsherpa01@gmail.com>
2025-02-11 14:21:53 +1100

    fixed loading issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/learning_react",
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+  },
+});
